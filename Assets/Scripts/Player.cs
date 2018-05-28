691e83795bb4ef439684a110ff9c01fe15f1bd98
Unknown <jaybritchie@gmail.com>
2018-05-28 17:48:24 -0400

    More ground stuff
    It's almost working I think

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -9,9 +9,23 @@ public class Player : MonoBehaviour
     private Rigidbody rb;
     private float freezeAxis = 0.0f;
 
+    private GameObject[] ground;
+
+    private Transform gPrev;
+    private Transform gCurrent;
+    private Transform gNext;
+
+    private int pGround;
+    private Transform groundT;
+
     private void Start()
     {
         rb = GetComponent<Rigidbody>();
+
+        ground = GameObject.FindGameObjectsWithTag("Ground");
+        gCurrent = ground[0].GetComponent<Transform>();
+        gNext = ground[1].GetComponent<Transform>();
+        pGround = 0;
     }
 
     private void FixedUpdate()
@@ -32,4 +46,27 @@ public class Player : MonoBehaviour
 
     }
 
+    //Move ground two back ahead of current
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "GroundT")
+        {
+            groundT = other.GetComponentInParent<Transform>();
+            pGround = Mathf.FloorToInt(rb.position.x / 20);
+            gCurrent = other.GetComponentInParent<Transform>();
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "GroundT")
+        {
+            pGround = Mathf.FloorToInt(rb.position.x / 20);
+            //Set previous to one we just exited
+            gNext = gPrev;
+            gPrev = other.GetComponentInParent<Transform>();
+
+            gNext.SetPositionAndRotation(new Vector3((pGround + 1) * 20, 0.0f, 0.0f), gNext.rotation);
+        }
+    }
 }
