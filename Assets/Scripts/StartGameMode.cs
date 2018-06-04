43e53fe321e8eb78a92242f06d0ee0ecbf926c86
Noel Eugenio <noel.shirodev@gmail.com>
2018-06-04 19:16:15 -0400

    Removed onclick for now
    Added functions for when buttons are clicked.

--- Assets/Scripts/StartGameMode.cs
+++ Assets/Scripts/StartGameMode.cs
@@ -1,13 +1,35 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine;
+using UnityEngine.UI;
+
 
 public class StartGameMode : MonoBehaviour
 {
+    public Button waveModeButton;
+    public Button infiniteModeButton;
 
-    public void ChooseGameMode(string scene)
+    void Start()
     {
-        SceneManager.LoadScene(scene);
+        Button wave = waveModeButton.GetComponent<Button>();
+        Button infinite = infiniteModeButton.GetComponent<Button>();
+
+        wave.onClick.AddListener(StartWaveMode);
+        infinite.onClick.AddListener(StartInfiniteMode);
     }
+
+    public void StartWaveMode()
+    {
+        Debug.Log("You chose wave mode");
+    }
+
+    public void StartInfiniteMode()
+    {
+        Debug.Log("You chose infinite mode");
+    }
+    //public void ChooseGameMode(string scene)
+    //{
+    //    SceneManager.LoadScene(scene);
+    //}
 }
