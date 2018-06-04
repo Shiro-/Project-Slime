35c02ae5d136396a610a38a999df35c57e65f17e
Noel Eugenio <noel.shirodev@gmail.com>
2018-06-04 19:00:40 -0400

    Added a main menu
    Currently all it does is open our other scene.
    Added the script for it.

---
+++ Assets/Scripts/StartGameMode.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartGameMode : MonoBehaviour
+{
+
+    public void ChooseGameMode(string scene)
+    {
+        SceneManager.LoadScene(scene);
+    }
+}
