81bb341bb5ba4697fd88fd5946bb9a260d674a19
Noel Eugenio <noel.shirodev@gmail.com>
2018-07-15 18:33:45 -0400

    Started work on pausing

---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+
+    public bool isPaused = false;
+
+    void Update()
+    {
+        
+    }
+
+    void PauseGame()
+    {
+        Time.timeScale = 0f;
+        isPaused = true;
+    }
+
+    void ResumeGame()
+    {
+        Time.timeScale = 1f;
+        isPaused = false;
+    }
+
+}
