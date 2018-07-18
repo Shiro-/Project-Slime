223c6408f1230d354dc9f90170a933c729e1d4b7
Noel Eugenio <noel.shirodev@gmail.com>
2018-07-17 20:59:50 -0400

    Moved death screen stuff to its own script
    Currently still not working

---
+++ Assets/Scripts/DeathScreen.cs
@@ -0,0 +1,104 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class DeathScreen : MonoBehaviour
+{
+    //Death Stuff
+    public GameObject deathScreen;
+    public Button retryButton;
+    public Button returnToMainMenuButton;
+    public Button quitButton;
+
+    bool isPlayerDead;
+    int startValue;
+
+    void Awake()
+    {
+        GameObject startControllerObject = GameObject.FindWithTag("StartController");
+        StartGameMode gameValue = startControllerObject.GetComponent<StartGameMode>();
+        startValue = gameValue.mode;
+    }
+
+    void Start()
+    {
+        Button retry = retryButton.GetComponent<Button>();
+        Button returnToMainMenu = returnToMainMenuButton.GetComponent<Button>();
+        Button quit = quitButton.GetComponent<Button>();
+
+        retry.onClick.AddListener(RetryLevel);
+        returnToMainMenu.onClick.AddListener(ReturnToMainMenu);
+        quit.onClick.AddListener(QuitGame);
+    }
+
+    void Update()
+    {
+        CheckIfPlayerDied();
+
+        if (isPlayerDead)
+        {
+            SetDeathScreenActivity();
+            Time.timeScale = 0f;
+        }
+        else
+        {
+            //do nothing?
+        }
+    }
+
+    void CheckIfPlayerDied()
+    {
+        if (GameObject.FindWithTag("Player") == null)
+        {
+            isPlayerDead = true;
+        }
+        else
+        {
+            isPlayerDead = false;
+        }
+    }
+
+    void SetDeathScreenActivity()
+    {
+        if (isPlayerDead == true)
+        {
+            deathScreen.SetActive(true);
+        }
+        else
+        {
+            deathScreen.SetActive(false);
+        }
+    }
+
+    void RetryLevel()
+    {
+        if (startValue == 1)
+        {
+            SetDeathScreenActivity();
+            Time.timeScale = 1f;
+            SceneManager.LoadScene("WaveDifficulty");
+        }
+        else if (startValue == 2)
+        {
+            SetDeathScreenActivity();
+            Time.timeScale = 1f;
+            SceneManager.LoadScene("InfiniteDifficulty");
+        }
+        else
+        {
+            Debug.Log("Somehow you reached here and you shouldn't be here.");
+        }
+    }
+
+    void ReturnToMainMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    void QuitGame()
+    {
+        Application.Quit();
+    }
+}
--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -10,12 +10,6 @@ public class GameController : MonoBehaviour
     public Text distText;
     public Text waveText;
 
-    //Death Stuff
-    public GameObject deathScreen;
-    public Button retryButton;
-    public Button returnToMainMenuButton;
-    public Button quitButton;
-
     int startValue;
 
     bool isPlayerDead;
@@ -30,13 +24,7 @@ public class GameController : MonoBehaviour
 
     void Start()
     {
-        Button retry = retryButton.GetComponent<Button>();
-        Button returnToMainMenu = returnToMainMenuButton.GetComponent<Button>();
-        Button quit = quitButton.GetComponent<Button>();
-
-        retry.onClick.AddListener(RetryLevel);
-        returnToMainMenu.onClick.AddListener(ReturnToMainMenu);
-        quit.onClick.AddListener(QuitGame);
+        
     }
 
     void Update()
@@ -50,71 +38,7 @@ public class GameController : MonoBehaviour
         {
             distText.text = "Distance: " + p.dist;
         }
-
-        CheckIfPlayerDied();
-
-        if(isPlayerDead)
-        {
-            SetDeathScreenActivity();
-            Time.timeScale = 0f;
-        }
-        else
-        {
-            //do nothing?
-        }
+ 
     }
-
-    void CheckIfPlayerDied()
-    {
-        if(GameObject.FindWithTag("Player") == null)
-        {
-            isPlayerDead = true;
-        }
-        else
-        {
-            isPlayerDead = false;
-        }
-    }
-
-    void SetDeathScreenActivity()
-    {
-        if(isPlayerDead == true)
-        {
-            deathScreen.SetActive(true);
-        }
-        else
-        {
-            deathScreen.SetActive(false);
-        }
-    }
-
-    void RetryLevel()
-    {
-        if(startValue == 1)
-        {
-            SetDeathScreenActivity();
-            Time.timeScale = 1f;
-            SceneManager.LoadScene("WaveDifficulty");
-        }
-        else if(startValue == 2)
-        {
-            SetDeathScreenActivity();
-            Time.timeScale = 1f;
-            SceneManager.LoadScene("InfiniteDifficulty");
-        }
-        else
-        {
-            Debug.Log("Somehow you reached here and you shouldn't be here.");
-        }
-    }
-
-    void ReturnToMainMenu()
-    {
-        SceneManager.LoadScene("MainMenu");
-    }
-
-    void QuitGame()
-    {
-        Application.Quit();
-    }
+  
 }
