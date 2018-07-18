33688316953759fb565f3311cd09d3d87efcbb8a
Noel Eugenio <noel.shirodev@gmail.com>
2018-07-17 20:00:34 -0400

    Adding death screen
    Still WIP, nothing really is working right now for it.

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class GameController : MonoBehaviour
 {
@@ -9,9 +10,17 @@ public class GameController : MonoBehaviour
     public Text distText;
     public Text waveText;
 
+    //Death Stuff
+    public GameObject deathScreen;
+    public Button retryButton;
+    public Button returnToMainMenuButton;
+    public Button quitButton;
+
     int startValue;
 
-    private void Awake()
+    bool isPlayerDead;
+
+    void Awake()
     {
         //This gets what version of the game is being played
         GameObject startControllerObject = GameObject.FindWithTag("StartController");
@@ -19,12 +28,18 @@ public class GameController : MonoBehaviour
         startValue = gameValue.mode;
     }
 
-    private void Start()
+    void Start()
     {
-     
+        Button retry = retryButton.GetComponent<Button>();
+        Button returnToMainMenu = returnToMainMenuButton.GetComponent<Button>();
+        Button quit = quitButton.GetComponent<Button>();
+
+        retry.onClick.AddListener(RetryLevel);
+        returnToMainMenu.onClick.AddListener(ReturnToMainMenu);
+        quit.onClick.AddListener(QuitGame);
     }
 
-    private void Update()
+    void Update()
     {
         if(startValue == 1)
         {
@@ -35,6 +50,71 @@ public class GameController : MonoBehaviour
         {
             distText.text = "Distance: " + p.dist;
         }
-        
+
+        CheckIfPlayerDied();
+
+        if(isPlayerDead)
+        {
+            SetDeathScreenActivity();
+            Time.timeScale = 0f;
+        }
+        else
+        {
+            //do nothing?
+        }
+    }
+
+    void CheckIfPlayerDied()
+    {
+        if(GameObject.FindWithTag("Player") == null)
+        {
+            isPlayerDead = true;
+        }
+        else
+        {
+            isPlayerDead = false;
+        }
+    }
+
+    void SetDeathScreenActivity()
+    {
+        if(isPlayerDead == true)
+        {
+            deathScreen.SetActive(true);
+        }
+        else
+        {
+            deathScreen.SetActive(false);
+        }
+    }
+
+    void RetryLevel()
+    {
+        if(startValue == 1)
+        {
+            SetDeathScreenActivity();
+            Time.timeScale = 1f;
+            SceneManager.LoadScene("WaveDifficulty");
+        }
+        else if(startValue == 2)
+        {
+            SetDeathScreenActivity();
+            Time.timeScale = 1f;
+            SceneManager.LoadScene("InfiniteDifficulty");
+        }
+        else
+        {
+            Debug.Log("Somehow you reached here and you shouldn't be here.");
+        }
+    }
+
+    void ReturnToMainMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    void QuitGame()
+    {
+        Application.Quit();
     }
 }
